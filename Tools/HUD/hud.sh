009975a56fe051d7fd7957791aa978b0eac1999f
Matt <matt@matt-ultra-ubuntu.(none)>
2014-08-02 00:52:34 +0200

    Recorrected hud.sh

--- Tools/HUD/hud.sh
+++ Tools/HUD/hud.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python mav_pihud.py --master=/dev/ttyAMA0 --baudrate=115200 --taget-system=55 --target-component=1 --source-system=251
+python mav_pihud.py --master=/dev/ttyAMA0 --baudrate=115200 --target-system=55 --target-component=1 --source-system=251
 
 
