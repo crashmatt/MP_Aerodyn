48bfbf43091d868c21c2acae7f251eb44da9c472
Matt <matt@matt-ultra-ubuntu.(none)>
2014-08-06 00:29:41 +0200

    soundgen running on x86+alsa with callback

--- Tools/HUD/soundgen.py
+++ Tools/HUD/soundgen.py
@@ -13,7 +13,7 @@ import sys
 CHUNK = 1024
 TABLE_LENGTH = 1024
 RATE = 11025
-CHANNELS = 1
+CHANNELS = 2
 
 print ("system platform = " + sys.platform)
 if sys.platform == 'darwin':
@@ -29,6 +29,14 @@ def sine(frequency, time, rate):
 
 
 def cb(in_data, frame_count, time_info, status):
+    flags = status
+#    print("in_data[0], in_data_len, frame count, status", in_data[0], len(in_data), frame_count, status)
+    if flags != 0:
+        if flags & pyaudio.paInputOverflow: print("Input Overflow")
+        if flags & pyaudio.paInputUnderflow: print("Input Underflow")
+        if flags & pyaudio.paOutputOverflow: print("Output Overflow")
+        if flags & pyaudio.paOutputUnderflow: print("Output Underflow")
+        if flags & pyaudio.paPrimingOutput: print("Priming Output")
     return myvario.callback(in_data, frame_count, time_info, status)
 
 class soundgen(object):
@@ -42,19 +50,26 @@ class soundgen(object):
         '''
         self.wave = sine(1.0, 1.0, RATE)
         self.phase = 0.0
-        self.frequency = 200.0
+        self.frequency = 300.0
         
-        self.gen_buffer = numpy.zeros(CHUNK, numpy.float32)
-        self.chunks = Queue.Queue(2)
+        self.gen_buffer = numpy.zeros(CHUNK * CHANNELS, numpy.float32)
+        self.chunks = Queue.Queue(3)
         self.first_chunk = True
-                
+        self.amplitude = 0.25
+        
         self.stream = p.open(format=pyaudio.paFloat32,
                                   channels=CHANNELS, rate=RATE, output=True, input=True, stream_callback=cb) #self.callback
             
     def callback(self, in_data, frame_count, time_info, status):
-        chunk = self.chunks.get(True, 1.0)
-        self.chunks.task_done()
-        return (chunk, pyaudio.paContinue)
+        print("in_data[0], in_data_len, frame count, status", in_data[0], len(in_data), frame_count, status)
+        try:
+            chunk = self.chunks.get_nowait()
+            self.chunks.task_done()
+            return (chunk, pyaudio.paContinue)
+        except:
+            print("no chunks available")
+            return ( in_data, pyaudio.paContinue)
+
 
     def gen_sound(self):
         phase_delta = self.frequency * (float(TABLE_LENGTH) / float(RATE) )
@@ -62,22 +77,31 @@ class soundgen(object):
             self.phase += phase_delta
             if(self.phase >= TABLE_LENGTH):
                 self.phase -= TABLE_LENGTH
-            self.gen_buffer[i] = self.wave[int(self.phase)]
+            self.gen_buffer[i*2] = self.wave[int(self.phase)] * self.amplitude
+            self.gen_buffer[(i*2)+1] = self.wave[int(self.phase)] * self.amplitude
+            time.sleep(0)   #yield to callback that needs to run quickly
+
         
         chunk = (self.gen_buffer.astype(numpy.float32).tostring())
-        self.chunks.put(chunk, True, 0.5)
-        
+        try:
+            self.chunks.put_nowait(chunk)
+        except:
+            print("not enough space in chunk queue")
+            
     def run(self):
-
-        for i in xrange(0,100):
-            if(self.chunks.empty()):
+        endtime = time.time() + 10
+        
+        while time.time() < endtime:
+            if(not self.chunks.full()):
                 self.gen_sound()
+                self.frequency += 1
             else:
-                time.sleep(0.05)
+                time.sleep(0.01)
     
 #            if(self.first_chunk == True):
-#                self.stream.start_stream()
 #                self.first_chunk = False
+            if(self.stream.is_active() == False):
+                self.stream.start_stream()
                
 #        self.stream.stop_stream()
 #        self.stream.close()
@@ -85,6 +109,12 @@ class soundgen(object):
         
         #self.stream.write(self.buffer.astype(numpy.float32).tostring()
         
+    def close(self):
+        self.stream.stop_stream()
+        self.stream.close()
+       
+        
 myvario = soundgen()
 myvario.run()
+myvario.close()
 p.terminate()
