f1b2d9479a251b962f7964e4dbe2799d9813b0c9
crashmatt <uavflightdirector@gmail.com>
2015-05-19 22:39:32 +0200

    HUD.py : Added angle filtering with rollover and estimate timeout to prevent runaway

--- Tools/HUD/HUD.py
+++ Tools/HUD/HUD.py
@@ -53,19 +53,22 @@ class LowPassFilter(object):
         else:
             time_now = timestamp
         delta_time = time_now - self.last_filter_time
-        const = self.const * delta_time
-        if const > 1.0:
-            const = 1.0;
-        self.output = (const * input) + ((1-const) * self.output)
+        coeff = self.const * delta_time
+        if coeff > 1.0:
+            coeff = 1.0;
+        self.output = (coeff * input) + ((1-coeff) * self.output)
         self.last_filter_time = time_now
         return self.output
+    
+    def set_filter(self, value):
+        self.output = value
     
     def get_output(self):
         return self.output;
     
 
 class Filter(object):
-    def __init__(self, damping=0.5, rate_gain=1.0, rate_damp=0.6):
+    def __init__(self, damping=0.5, rate_gain=1.0, rate_damp=0.6, rate_decay=0.5, max_deltatime=1.0):
         self.damping = damping;
         self.rate_gain = rate_gain;
         
@@ -75,19 +78,65 @@ class Filter(object):
                 
         self.system_timestamp = 0
         self.measurement_timestamp = 0
+        self.max_deltatime = max_deltatime
         
     def observation(self, value, rate, measurement_timestamp, system_timestamp):
-        self.input_value = value
-        self.rate_filter.run_filter(rate, measurement_timestamp)
-        self.system_timestamp = system_timestamp
-        self.system_timestamp = system_timestamp
+        if(measurement_timestamp != self.measurement_timestamp):    # Filter identical measurements
+            self.input_value = value
+            self.rate_filter.run_filter(rate, measurement_timestamp)
+            self.measurement_timestamp = measurement_timestamp
+            self.system_timestamp = system_timestamp
                 
     def estimate(self, deltatime=0):
         if(deltatime == 0):
             deltatime = time.time() - self.system_timestamp
-            estimate = self.input_value + (deltatime * self.rate_filter.get_output() * self.rate_gain)
+            if(deltatime > self.max_deltatime):
+                deltatime = self.max_deltatime
+            coeff = deltatime * self.rate_gain            
+            estimate = self.input_value + (self.rate_filter.get_output() * coeff)
         return self.output_filter.run_filter(estimate)
     
+
+class AngleFilter(Filter):
+    def __init__(self, damping=0.5, rate_gain=1.0, rate_damp=0.6, rate_decay=0.5, degrees=True):
+        Filter.__init__(self, damping, rate_gain, rate_damp, rate_decay)
+        self.degrees = degrees
+        
+        if(degrees == False):
+            print("RADIANS NOT YET SUPPORTED, DOING DEGREES")
+        
+    def observation(self, value, rate, measurement_timestamp, system_timestamp):
+        # precondition value so it is never more than 360 degrees away from last filter output
+        output = self.output_filter.get_output()
+        if(value >= output + 360):
+            precond = value - 360
+        elif(value <= output - 360):
+            precond = value + 360
+        else:
+            precond = value
+        
+        Filter.observation(self, precond, rate, measurement_timestamp, system_timestamp)
+        
+        # Adjust filter angle rollover
+        output = self.output_filter.get_output()
+        if(output > 180):
+            self.output_filter.set_filter(output-360)
+        elif(output < -180):
+            self.output_filter.set_filter(output+360)
+            
+    def estimate(self, deltatime=0):
+        outval = Filter.estimate(self, deltatime)
+
+        # correct for angle rollover
+        if(outval > 180.0):
+            outval = outval - 360
+            self.output_filter.set_filter(outval-360.0)
+        elif(outval < -180.0):
+            outval = outval +360
+            self.output_filter.set_filter(outval+360.0)
+            
+        return outval
+
 
 class HUD(object):
     def __init__(self, simulate=False, master=True, update_queue=None):
@@ -164,8 +213,8 @@ class HUD(object):
         self.slip = 0               #slip in degrees
         self.mode = "FBW"
         
-        self.pitch_filter = Filter(damping=0.75, rate_gain = 0.5)
-        self.roll_filter = Filter(damping=0.75, rate_gain = 0.0)
+        self.pitch_filter = AngleFilter(damping=0.75, rate_gain = 0.5)
+        self.roll_filter = AngleFilter(damping=0.75, rate_gain = 0.0)
 
         
 #        self.climb_rate = 0
