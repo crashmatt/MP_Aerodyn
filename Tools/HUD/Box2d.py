385a19dca523a1db06a1c33e27d0df230e48bf27
crashmatt <uavflightdirector@gmail.com>
2015-09-08 00:35:56 +0200

    Drawing coloured 2d boxes using CPlanes. No alpha control yet.

--- Tools/HUD/Box2d.py
+++ Tools/HUD/Box2d.py
@@ -2,14 +2,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from pi3d.constants import *
 from pi3d import Shape, Shader
-#from pi3d.shape import Plane as Plane
+from pi3d.shape import Plane as Plane
 import CPlanes
+#from pi3d import Plane
 
 class Box2d(object):
     """ 3d model inherits from Shape"""
     def __init__(self,  camera=None, light=None, w=1.0, h=1.0, d=1.0,
                name="", x=0.0, y=0.0, z=0.0,
-               line_colour=(255,255,255,255), fill_colour=(0,0,0,255), line_thickness = 1,
+               line_colour=(1.0,1.0,1.0,1.0), fill_colour=(0,0,0,1.0), line_thickness = 1,
                shader=None, justify='C'):
         """uses standard constructor for Shape extra Keyword arguments:
 
@@ -39,38 +40,69 @@ class Box2d(object):
         else:
             xoffset = int(x)
 
-        if(fill_colour[3] > 0):
-            self.box = CPlanes.CPlanes(camera=camera, w=w, h=h, x=xoffset, y=y, z=z, colour=fill_colour[0:3])
- #           self.box.set_material((fill_colour))
-            self.box.set_draw_details(self.shader, [], 0, 0)
-            self.box.set_alpha(fill_colour[3])
+        #=======================================================================
+        # self.box = Plane.Plane(camera=camera, x=xoffset, y=y, z=z, h=h, w=w)
+        # self.box.set_draw_details(self.shader, [], 0, 0)
+        # self.box.set_material(fill_colour)
+        # self.box.set_alpha(fill_colour[3])
+        #=======================================================================
+        
+        self.box = CPlanes.CPlanes(camera=camera, x=xoffset, y=y, z=z)
+        ww = w/2.0
+        hh = h/2.0
+          
+        #pts = ( (xoffset-ww, y-hh, 0),(xoffset+ww, y-hh, 0),(xoffset+ww, y+hh, 0),(xoffset-ww, y+hh, 0) ) 
+        
+        pts = ((-ww, hh, 0.0), (ww, hh, 0.0), (ww, -hh, 0.0), (-ww, -hh, 0.0))          
+        self.box.add_plane(pts, fill_colour)
+#        self.box.set_alpha(fill_colour[3])
 
-        self.boxtop = CPlanes.CPlanes(camera=camera, w=w+(line_thickness*2), h=line_thickness, x=xoffset, y=y+(h/2)+(line_thickness/2), z=z, colour=line_colour[0:3])
-#        self.boxtop.set_material((line_colour))
-        self.boxtop.set_draw_details(self.shader, [], 0, 0)
-        self.boxtop.set_alpha(line_colour[3])
+        #order of points is critial to show face. Effectively a sprite with hidden back face
 
-        self.boxbottom = CPlanes.CPlanes(camera=camera, w=w+(line_thickness*2), h=line_thickness, x=xoffset, y=y-(h/2)-(line_thickness/2), z=z, colour=line_colour[0:3])
-#        self.boxbottom.set_material((line_colour))
-        self.boxbottom.set_draw_details(self.shader, [], 0, 0)
-        self.boxbottom.set_alpha(line_colour[3])
+        #top
+        pts = ((-ww, hh+line_thickness, 0.0), (ww, hh+line_thickness, 0.0), (ww, hh, 0.0), (-ww, hh, 0.0))                  
+        self.box.add_plane(pts, line_colour)
 
-        self.boxleft = CPlanes.CPlanes(camera=camera, w=line_thickness, h=h+(line_thickness*2), x=xoffset-(w/2)-(line_thickness/2), y=y, z=z, colour=line_colour[0:3])
-#        self.boxleft.set_material((line_colour))
-        self.boxleft.set_draw_details(self.shader, [], 0, 0)
-        self.boxleft.set_alpha(line_colour[3])
+        #bottom
+        pts = ((-ww, -hh, 0.0), (ww, -hh, 0.0), (ww, -hh-line_thickness, 0.0), (-ww, -hh-line_thickness, 0.0))                  
+        self.box.add_plane(pts, line_colour)
 
-        self.boxright = CPlanes.CPlanes(camera=camera, w=line_thickness, h=h+(line_thickness*2), x=xoffset+(w/2)+(line_thickness/2), y=y, z=z, colour=line_colour[0:3])
-#        self.boxright.set_material((line_colour))
-        self.boxright.set_draw_details(self.shader, [], 0, 0)
-        self.boxright.set_alpha(line_colour[3])
+        #left
+        pts = ((-ww-line_thickness, hh, 0.0), (-ww, hh, 0.0), (-ww, -hh, 0.0), (-ww-line_thickness, -hh, 0.0))
+        self.box.add_plane(pts, line_colour)
+        
+        pts = ((ww, hh, 0.0), (ww+line_thickness, hh, 0.0), (ww+line_thickness, -hh, 0.0), (ww, -hh, 0.0))
+        self.box.add_plane(pts, line_colour)
+        
+        self.box.set_draw_details(self.shader, [], 0, 0)
+        self.box.init()
+
+
+#        if(fill_colour[3] > 0):
+#            self.box = CPlanes.CPlanes(camera=camera, w=w, h=h, x=xoffset, y=y, z=z, colour=fill_colour[0:3])
+
+#        self.boxtop = CPlanes.CPlanes(camera=camera, w=w+(line_thickness*2), h=line_thickness, x=xoffset, y=y+(h/2)+(line_thickness/2), z=z, colour=line_colour[0:3])
+#        self.boxtop.set_draw_details(self.shader, [], 0, 0)
+#        self.boxtop.set_alpha(line_colour[3])
+
+#        self.boxbottom = CPlanes.CPlanes(camera=camera, w=w+(line_thickness*2), h=line_thickness, x=xoffset, y=y-(h/2)-(line_thickness/2), z=z, colour=line_colour[0:3])
+#        self.boxbottom.set_draw_details(self.shader, [], 0, 0)
+#        self.boxbottom.set_alpha(line_colour[3])
+
+#        self.boxleft = CPlanes.CPlanes(camera=camera, w=line_thickness, h=h+(line_thickness*2), x=xoffset-(w/2)-(line_thickness/2), y=y, z=z, colour=line_colour[0:3])
+#        self.boxleft.set_draw_details(self.shader, [], 0, 0)
+#        self.boxleft.set_alpha(line_colour[3])
+
+#        self.boxright = CPlanes.CPlanes(camera=camera, w=line_thickness, h=h+(line_thickness*2), x=xoffset+(w/2)+(line_thickness/2), y=y, z=z, colour=line_colour[0:3])
+#        self.boxright.set_draw_details(self.shader, [], 0, 0)
+#        self.boxright.set_alpha(line_colour[3])
         
     def draw(self):
         box = getattr(self, "box", None) 
         if(box != None):
             box.draw()
-        self.boxtop.draw()
-        self.boxbottom.draw()
-        self.boxleft.draw()
-        self.boxright.draw()
+ #       self.boxtop.draw()
+ #       self.boxbottom.draw()
+ #       self.boxleft.draw()
+ #       self.boxright.draw()
 
--- Tools/HUD/CPlanes.py
+++ Tools/HUD/CPlanes.py
@@ -3,14 +3,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from pi3d.constants import *
 from pi3d.Buffer import Buffer
 from pi3d.Shape import Shape
+import numpy as np
+from numpy import dtype
 
 class CPlanes(Shape):
   """ Draws a group of disconnected coloured planes. Uses normals as colour and no textures. Only flat material shaders and 2d cameras can be used"""
-  def __init__(self, camera=None, light=None, w=1.0, h=1.0, name="",
+  def __init__(self, camera=None, light=None, name="",
                x=0.0, y=0.0, z=0.0,
                rx=0.0, ry=0.0, rz=0.0,
                sx=1.0, sy=1.0, sz=1.0,
-               cx=0.0, cy=0.0, cz=0.0, colour=(1.0, 0.0, 0.0)):
+               cx=0.0, cy=0.0, cz=0.0):
     """uses standard constructor for Shape extra Keyword arguments:
 
       *w*
@@ -24,22 +26,41 @@ class CPlanes(Shape):
     if VERBOSE:
       print("Creating plane ...")
 
-    self.width = w
-    self.height = h
     self.ttype = GL_TRIANGLES
-    self.verts = []
-    self.norms = []
+    self.verts = np.zeros((0,3), dtype=np.float)
+    self.norms = np.zeros((0,3), dtype=np.float)
     self.texcoords = []
-    self.inds = []
+    self.inds = np.zeros((0,3), dtype=np.int)
+    self.planes = []
 
-    ww = w / 2.0
-    hh = h / 2.0
-
-    self.verts = ((-ww, hh, 0.0), (ww, hh, 0.0), (ww, -hh, 0.0), (-ww, -hh, 0.0))
-    self.norms = (colour, colour,  colour, colour)
 #    self.texcoords = ((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
 
-    self.inds = ((0, 1, 3), (1, 2, 3))
 
     self.buf = []
+
+  def add_plane(self, pts, colour):
+    norms = np.zeros((4,3), dtype=np.float)
+    verts = np.zeros((4,3), dtype=np.float)
+    inds = np.zeros((2,3), dtype=np.float)
+    ind_count = len(self.verts)
+    for index in range(0,4):
+        verts[index] = [ pts[index][0], pts[index][1], pts[index][2] ]
+        norms[index] = [colour[0], colour[1], colour[2]]
+    inds = [ [ind_count, ind_count+1, ind_count+3], [ind_count+1, ind_count+2, ind_count+3] ]
+    self.verts = np.append(self.verts, verts, axis=0)
+    self.norms = np.append(self.norms, norms, axis=0)
+    self.inds = np.append(self.inds, inds, axis=0)
+    
+#===============================================================================
+#     ww = 50
+#     hh = 50
+# 
+#     self.verts = ((-ww, hh, 0.0), (ww, hh, 0.0), (ww, -hh, 0.0), (-ww, -hh, 0.0))
+#===============================================================================
+#    self.norms = ((0.0, 0.0, -1), (0.0, 0.0, -1),  (0.0, 0.0, -1), (0.0, 0.0, -1))
+#    self.texcoords = ((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
+
+#    self.inds = ((0, 1, 3), (1, 2, 3))
+
+  def init(self):
     self.buf.append(Buffer(self, self.verts, self.texcoords, self.inds, self.norms))
